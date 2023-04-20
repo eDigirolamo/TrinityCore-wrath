044c50c533a0eb012c17bba4d847935f3baa8fad
Edward DiGirolamo <edward.j.digirolamo@gmail.com>
2023-04-20 17:46:06 -0400

    Delete src/tools directory

--- src/tools/contrib/npcbot stats and gear/07 populate table characters_npcbot_class_role.sql
+++
@@ -1,8 +0,0 @@
--- 
-
--- existing bots
-TRUNCATE TABLE `characters_npcbot_class_role`;
-INSERT INTO `characters_npcbot_class_role` (`entry`) (SELECT `entry` FROM `characters_npcbot`);
-
--- trigger `characters_npcbot_class_role`.`class_roles` update
-UPDATE `characters_npcbot` SET `roles`=`roles`;
